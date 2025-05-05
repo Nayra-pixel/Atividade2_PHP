1599a1d1988ffc2340f43461bf713da276165ccf
Nayra-pixel <nayrasduarte@gmail.com>
2025-05-05 20:20:25 -0300

    segundo

--- primeiro.php
+++ primeiro.php
@@ -1,14 +1,19 @@
 <?php 
-	$n = 1; //entrada do número
-	$t = 0;
+	$n = 10; //entrada do número
 	$maior = 0;
+	$menor = 0;
 
 	
 	
 	while($n != 0){
-	
-	
+		if($n > $maior){
+			$maior = $n;
+		}elseif($n == 0){
+			break;
+		}
 	}
+
+	echo ("O maior número digitado foi " . $maior);
 	
 
  ?>
