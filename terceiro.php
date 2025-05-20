761dac1bff4bca9ff296af4579e45478f7b20096
Nayra-pixel <nayrasduarte@gmail.com>
2025-05-20 19:45:30 -0300

    finalizado

--- terceiro.php
+++ terceiro.php
@@ -1,15 +1,15 @@
 <?php 
 
 	$grao = 1;
-	$cont = 0;
-	$graocasa = 0;
+	$cont = 2;
+	$graosoma = 1;
 
 	while($cont <= 64){
-
-		$grao = $grao + ($grao * 2);
+		$grao = $grao * 2;
+		$graosoma = $grao + $graosoma;
 		$cont++;
-
 	}
 
-	echo "O monge esperava receber: " . $grao;
+	echo "O monge esperava receber: " . $graosoma;
+
  ?>
